826c3d83e437ccb11b6c9ca64eee2687371a51f8
sulfe <song1124@gmail.com>
2022-05-17 11:47:33 +0900

    Update Main_gui.py

--- src/Main_gui.py
+++ src/Main_gui.py
@@ -1,6 +1,10 @@
+#-*- coding:utf-8 -*-
+
 from optparse import Values
+import threading
 from tkinter import *
 from tkinter import filedialog
+from tkinter import font
 import tkinter.ttk as ttk
 import os
 import crolling
@@ -15,6 +19,11 @@ def openFile():
     ('exe files', '*.exe'),('all files', '*.*')))
     driverEnt.delete(0,None)
     driverEnt.insert(0,dir_file)
+    
+def th():
+    th = threading.Thread(target=downAct)
+    th.daemon = True
+    th.start()
     
 def downAct():
     optionPhoto = [searchEnt.get(),engine.get(),driverEnt.get(),rootEnt.get(),downNumCom.get()]
@@ -51,25 +60,25 @@ driverBtn = Button(root,text='찾아보기',width=10,height=1,command=openFile) 
 driverEnt = Entry(root,width=40) #드라이버 엔트리
 
 openBtn = Button(root,text='경로 열기',width=20,height=1,command=openPath) #경로열기 버튼
-downBtn = Button(root,text='다운로드',width=20,height=1,command=downAct) #다운로드 버튼
+downBtn = Button(root,text='다운로드',width=20,height=1,command=th) #다운로드 버튼
 
 #test출력 라벨 optionPhoto 확인용
 #testLabel = Label(root,text=' ', width=80, height=1)
 #testLabel.place(x=20,y=280)
 
-searchlabel.place(x=30,y=14) #검색 라벨 위치
-enginelabel.place(x=20,y=65) #엔진 선택 라벨 위치
-rootlabel.place(x=13,y=163) #다운로드 경로 라벨 위치
-numlabel.place(x=13,y=214) #다운로드 개수 라벨 위치
-searchEnt.place(x=100,y=30) #검색 엔트리 위치
-engine.place(x=100,y=80) #검색 엔진 콤보박스 위치
-rootEnt.place(x=100,y=180) #경로 출력 엔트리 위치
-rootBtn.place(x=395,y=177) #경로 출력 설정 버튼 위치
-downNumCom.place(x=100,y=230) #다운로드 개수 지정 콤보박스 위치
+searchlabel.place(x=41,y=34) #검색 라벨 위치
+enginelabel.place(x=31,y=85) #엔진 선택 라벨 위치
+rootlabel.place(x=23,y=183) #다운로드 경로 라벨 위치
+numlabel.place(x=23,y=234) #다운로드 개수 라벨 위치
+searchEnt.place(x=120,y=50) #검색 엔트리 위치
+engine.place(x=120,y=100) #검색 엔진 콤보박스 위치
+rootEnt.place(x=120,y=200) #경로 출력 엔트리 위치
+rootBtn.place(x=415,y=197) #경로 출력 설정 버튼 위치
+downNumCom.place(x=120,y=250) #다운로드 개수 지정 콤보박스 위치
 openBtn.place(x=75,y=330) #경로열기 버튼 위치
-downBtn.place(x=315,y=330) #다운로드 버튼 위치
-driverlabel.place(x=13,y=114) #드라이버 지정 라벨 위치
-driverBtn.place(x=395,y=125) #드라이버 지정 버튼 위치
-driverEnt.place(x=100,y=130) #드라이버 지정 엔트리 위치
+downBtn.place(x=320,y=330) #다운로드 버튼 위치
+driverlabel.place(x=23,y=134) #드라이버 지정 라벨 위치
+driverBtn.place(x=415,y=145) #드라이버 지정 버튼 위치
+driverEnt.place(x=120,y=150) #드라이버 지정 엔트리 위치
 
 root.mainloop()
